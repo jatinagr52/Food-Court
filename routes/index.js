69377dd505ca61e8540042c1b7c52355fa7c704a
jatin <avr.jatin@gmail.com>
2018-03-20 14:50:11 +0530

    BC changes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var firebase=require('firebase');
 var router = express.Router();
-//var model=require('../model/collector');
-/* GET home page. */
+var fb= require('firebase-nodejs');
+var web3 =require('web3');
+var myweb=new web3(new web3.providers.HttpProvider('http://localhost:8545'));
+var abi=[ { "constant": false, "inputs": [ { "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "approve", "outputs": [ { "name": "success", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transferFrom", "outputs": [ { "name": "success", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" } ], "name": "balanceOf", "outputs": [ { "name": "balance", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transfer", "outputs": [ { "name": "success", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" } ], "name": "allowance", "outputs": [ { "name": "remaining", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "_from", "type": "address" }, { "indexed": true, "name": "_to", "type": "address" }, { "indexed": false, "name": "_value", "type": "uint256" } ], "name": "Transfer", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "_owner", "type": "address" }, { "indexed": true, "name": "_spender", "type": "address" }, { "indexed": false, "name": "_value", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "name", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [ { "name": "", "type": "uint8" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [ { "name": "", "type": "address" } ], "name": "balances", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" } ], "name": "balanceOf", "outputs": [ { "name": "balance", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [ { "name": "", "type": "address" }, { "name": "", "type": "address" } ], "name": "allowed", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" } ], "name": "allowance", "outputs": [ { "name": "remaining", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "_owner", "type": "address" }, { "indexed": true, "name": "_spender", "type": "address" }, { "indexed": false, "name": "_value", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "_from", "type": "address" }, { "indexed": true, "name": "_to", "type": "address" }, { "indexed": false, "name": "_value", "type": "uint256" } ], "name": "Transfer", "type": "event" }, { "constant": false, "inputs": [ { "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "approve", "outputs": [ { "name": "success", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transfer", "outputs": [ { "name": "success", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transferFrom", "outputs": [ { "name": "success", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "name": "_initialAmount", "type": "uint256" }, { "name": "_tokenName", "type": "string" }, { "name": "_decimalUnits", "type": "uint8" }, { "name": "_tokenSymbol", "type": "string" } ], "payable": false, "stateMutability": "nonpayable", "type": "constructor" } ];
+
+var addr="0x21d874d519f4277a7d46fd91616cc4f6238f8d93";
+
+var myContract= new myweb.eth.Contract(abi,'0x21d874d519f4277a7d46fd91616cc4f6238f8d93');
+ //var myContract =new myweb.eth.Contract(abi).at(addr);
+//console.log(myweb.eth.Contract);
+
+//var myContract=new web3.eth.Contract(abi,)
+//console.log(myContract);
 firebase.initializeApp({
   apiKey: "AIzaSyD8bVeIajJkBAGa1fliiESlKj5FsIVKqbE",
     authDomain: "food-court-9abb2.firebaseapp.com",
@@ -15,6 +26,9 @@ var database = firebase.database();
 var ref=database.ref('shop');
 var addRef= ref.child('register');
 var refC=database.ref('Customer');
+
+
+
 router.get('/', function(req, res, next) {
   res.render('index');
 });
@@ -34,7 +48,7 @@ router.get('/transfer',function(req,res,next){
   res.render('transfer');
 });
 router.get('/success',function(req,res,next){
-  console.log("RENDERING>>>");
+
   res.render('success');
 });
 router.get('/success1',function(req,res,next){
@@ -46,23 +60,30 @@ router.post('/login',function(req,res,next){
     res.redirect('/collector');
   }
 });
-
+/* -------------------------------------------------------------run when connect-------------------*/
+// var newAcc;
+// refC.on('child_added',function(result){
+//     var newAcc=result.val().password;
+//     console.log(newAcc);
+//     var newAccAddr=myweb.eth.personal.newAccount(newAcc);
+//      refC.push({
+//        address:newAccAddr,
+//      });
+// });
 router.post('/collector',function(req,res,next){
+refC.on('value',function(snapshot){
+  if(req.body.cid==snapshot.val().ID){
+    res.redirect('/transfer');
+  }
+  else{
+    res.redirect('/collector');
+    console.log("invalid userid");
+  }
+})
 
-    refC.on("value",function(obj){
-      console.log(obj.value());
-    });
-
-  //console.log(req.body.cid);
-  // if(req.body.cid=="jatin"){
-  //   res.redirect('/transfer');
-  // }
-  // else{
-  //   res.redirect('/collector');
-  // }
 });
 router.post('/register',function(req,res,next){
- console.log("ja rha h iss loop m");
+
  addRef.push({
    sname: req.body.sname,
    id: req.body.sid,
@@ -71,12 +92,32 @@ router.post('/register',function(req,res,next){
    email: req.body.email,
    phone: req.body.phn
  });
- console.log('this is running...');
+
  res.redirect('/login');
 });
+
+var myAddr;
 router.post('/transfer',function(req,res,next){
-  res.redirect('home/hadoop/final/food/views/success');
-})
+  myContract.methods.balanceOf(myweb.eth.coinbase).call({from:myweb.eth.coinbase},function(err,res){
+
+    console.log(res);
+  });
+refC.on('value',function(res){
+ myAddr=res.val();
+});
+myConract.transfer(myAddr.address,req.body.token).send({from:myweb.eth.coinbase},function(err,txhash){
+  console.log(err);
+});
+
+ var transaction{
+   from: "admin",
+   to:
+ }
+
+  // myWeb.personal.unlockAccount(myweb.eth.coinbase);
+  // console.log(myContract.balanceOf("0xfb09bbbefc159fa1490dec05aa7e97a8e69862f2"));
+
+});
 
 
 module.exports = router;
